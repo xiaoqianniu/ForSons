427308a7ee7f06c9c6506b62929ee79fc4f0b768
xiaoqianniu <xiaoqianniu@gmail.com>
2018-05-08 10:28:42 -0400

    Initial Commit

---
+++ ForSons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForSons
+//
+//  Created by Hongbo Niu on 2018-05-08.
+//  Copyright © 2018 Hongbo Niu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
