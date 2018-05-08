8499aecd45b19f6af9e3bb468ddf5903b3c37e6c
xiaoqianniu <xiaoqianniu@gmail.com>
2018-05-08 11:34:13 -0400

    register custom cell and add tableview datasource methods

--- ForSons/Controller/Custom Cell/RulesTableViewCell.swift
+++ ForSons/Controller/Custom Cell/RulesTableViewCell.swift
@@ -12,9 +12,12 @@ class RulesTableViewCell: UITableViewCell {
 
     @IBOutlet weak var listLbl: UILabel!
     @IBOutlet weak var checkBtn: UIButton!
+    
+ 
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- ForSons/Controller/RulesCheckViewController.swift
+++ ForSons/Controller/RulesCheckViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class RulesCheckViewController: UIViewController {
+class RulesCheckViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        tableView.delegate = self
+        tableView.dataSource = self
+        let nibName = UINib(nibName: "RulesCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "listCell")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath) as! RulesTableViewCell
+        return cell
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90.0
+    }
 
 }
 
